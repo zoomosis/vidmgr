e7d7112ab29eddc7089214cd60de5c358d329b93
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 03:53:23 +1100

    Initial import

---
+++ qmenu/compile.bat
@@ -0,0 +1,7 @@
+rem make -f makefile.dos
+rem nmake -f makefile.qcc
+wmake -h -u -f makefile.w16
+rem wmake -h -u -f makefile.w32
+rem wmake -h -u -f makefile.wnt
+rem dmake -B -f makefile.djg
+rem dmake -B -f makefile.emx
